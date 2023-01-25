simplyIeaf/obs-studio

commit f96ae2d6c4b00ea9a34de8205f514db30c7e5b28

libobs: Open a separate X11 connection for hotkeys
Qt will mask certain input events. In order to avoid them masking things
like mouse button events we open a new display here.

Fixes #4843

libobs/obs-nix-x11.c
@@ -828,7 +828,8 @@ static inline void registerMouseEvents(struct obs_core_hotkeys *hotkeys)
 
 static bool obs_nix_x11_hotkeys_platform_init(struct obs_core_hotkeys *hotkeys)
 {
-	Display *display = obs_get_nix_platform_display();
+	// Open a new X11 connection here, this avoids Qt masking events we care about.
+	Display *display = XOpenDisplay(NULL);
 	if (!display)
 		return false;
 

commit 583d45c464be0b9b13bcb627cc486241cb49d741

libobs: Close display when destroying X11 hotkey platform
Currently, the X11 display is leaked when the X11 hotkey
platform is destroyed. Fix that by calling XCloseDisplay()
to free it.

Fixes: f96ae2d6c4b00e ("libobs: Open a separate X11 connection for hotkeys")

libobs/obs-nix-x11.c
@@ -878,6 +878,7 @@ static void obs_nix_x11_hotkeys_platform_free(struct obs_core_hotkeys *hotkeys)
 		da_free(context->keycodes[i].list);
 
 	bfree(context->keysyms);
+	XCloseDisplay(context->display);
 	bfree(context);
 
 	hotkeys->platform_context = NULL;